305923bda3e8c22cb04f0f20b941aac282ec6ec1
Antonio Fernandes <fernandes.anttonio@gmail.com>
2019-06-10 13:17:31 -0300

    Lista 9

--- Lista_9.R
+++ Lista_9.R
@@ -80,4 +80,115 @@ plot(Linear, which=1, col=c("blue")) # Q-Q plot
 hist(shortleaf$Diam)
 hist(shortleaf$Vol)
 
-  
+  # logaritmo das duas variáveis
+
+shortleaf$diam_log <- log(shortleaf$Diam)
+shortleaf$Vol_log <- log(shortleaf$Vol)  
+
+  # modelo linear
+
+Linear <- lm(Vol_log ~ diam_log, data = shortleaf)
+
+  # resumo do modelo
+
+summary(Linear)
+
+  # ajuste do modelo
+
+plot(shortleaf$diam_log, shortleaf$Vol_log)
+abline(lm(Vol_log ~ diam_log, data = shortleaf), col = 'red')
+
+plot(Linear, which=2, col=c("red")) # Residuals vs Fitted Plot
+
+plot(Linear, which=1, col=c("blue")) # Q-Q plot
+
+###### 4.1 c) ######################################################################
+
+mammget <- read_excel("mammget.xlsx", col_types = c("numeric","numeric"))
+
+# modelo linear
+
+Linear <- lm(length ~ birtwgt, data = mammget)
+
+# resumo do modelo
+
+summary(Linear)
+
+# ajuste do modelo
+
+plot(mammget$birtwgt, mammget$length)
+abline(lm(length ~ birtwgt, data = mammget), col = 'red')
+
+plot(Linear, which=2, col=c("red")) # Residuals vs Fitted Plot
+
+plot(Linear, which=1, col=c("blue")) # Q-Q plot
+
+
+# logaritmo da variável
+
+mammget$birtwgt_log <- log(mammget$birtwgt)
+
+# modelo linear
+
+Linear <- lm(length ~ birtwgt_log, data = mammget)
+
+# resumo do modelo
+
+summary(Linear)
+
+# ajuste do modelo
+
+plot(mammget$birtwgt_log, mammget$length)
+abline(lm(length ~ birtwgt_log, data = mammget), col = 'red')
+
+plot(Linear, which=2, col=c("red")) # Residuals vs Fitted Plot
+
+plot(Linear, which=1, col=c("blue")) # Q-Q plot
+
+
+###### 4.2 b) ######################################################################
+
+# lendo banco bluegills
+
+bluegills <- read.delim("~/Dados/Listas/AD_9/AD_9/bluegills.txt")
+
+# modelo linear
+
+Linear <- lm(length ~ age, data = bluegills)
+
+# resumo do modelo
+
+summary(Linear)
+
+# ajuste do modelo
+
+plot(bluegills$age, bluegills$length)
+abline(lm(length ~ age, data = bluegills), col = 'red')
+
+plot(Linear, which=2, col=c("red")) # Residuals vs Fitted Plot
+
+plot(Linear, which=1, col=c("blue")) # Q-Q plot
+
+# elevando a VI ao quadrado
+
+bluegills$age_sq <- bluegills$age^2
+
+# rodando o modelo com a vi ao quadrado
+
+Linear <- lm(length ~ age + age_sq, data = bluegills)
+
+# modelo
+
+summary(Linear)
+
+# ajuste do modelo
+
+hist(Linear$residuals)
+
+# resultado do modelo
+
+Linear$fitted.values
+
+
+
+
